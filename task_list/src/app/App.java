76dc9d06915e08d6fbed75975c2cdab22cea2cb0
Dylan Reichelt <dylan.reichelt1@gmail.com>
2019-04-07 12:30:48 -0700

    added basic window

--- task_list/src/app/App.java
+++ task_list/src/app/App.java
@@ -1,7 +1,24 @@
 package app;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String version = System.getProperty("java.version");
+        Label l = new Label ("Hello, JavaFX 11, running on "+version);
+        Scene scene = new Scene (new StackPane(l), 300, 200);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
