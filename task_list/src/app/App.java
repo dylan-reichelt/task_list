d23d391e1095ba1c5c0e975fa3aec8c30826bdbc
Dylan Reichelt <dylan.reichelt1@gmail.com>
2019-04-07 19:44:01 -0700

    Just testing with window placement

--- task_list/src/app/App.java
+++ task_list/src/app/App.java
@@ -2,18 +2,36 @@ package app;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Rectangle2D;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
+    	ObservableList<String> tasks = FXCollections.observableArrayList();
+    	ListView<String> manifest;
+    	manifest.setItems(tasks);
+    	
         String version = System.getProperty("java.version");
-        Label l = new Label ("Hello, JavaFX 11, running on "+version);
-        Scene scene = new Scene (new StackPane(l), 300, 200);
+        Label l = new Label("Hello, JavaFX 11, running on "+version);
+        Scene scene = new Scene (new StackPane(l));
+        
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        //set Stage boundaries to visible bounds of the main screen
+        stage.setX((primaryScreenBounds.getWidth() - 640) / 2);
+        stage.setY((primaryScreenBounds.getHeight() - 480) / 2);
+        stage.setWidth(primaryScreenBounds.getWidth()/3);
+        stage.setHeight(primaryScreenBounds.getHeight()/3);
         stage.setScene(scene);
+        stage.setTitle("TMI"); //TMI For Task Management Interface
         stage.show();
     }
 
